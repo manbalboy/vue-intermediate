5de141e27f3316efd3bb06ae59f72960ec50441e
manbalboy <manbalboy@hanmail.net>
2021-02-14 20:45:53 +0900

    할일 기능 추가

--- vue-todo-manbalboy/src/App.vue
+++ vue-todo-manbalboy/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <TodoHeader></TodoHeader>
-      <TodoInput></TodoInput>
+      <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
       <TodoList v-bind:propsdata="todoItems"></TodoList>
       <TodoFooter></TodoFooter>
   </div>
@@ -19,6 +19,14 @@
                 todoItems : []
             }
         }, 
+
+        methods: {
+            addOneItem : function  (todoItem) {
+                var obj = {completed: false, item: todoItem}
+                localStorage.setItem(todoItem, JSON.stringify(obj));
+                this.todoItems.push(obj);
+            }
+        },
 
         created : function () {
             if (localStorage.length > 0) {
