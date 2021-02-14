b5ecff3a346a4e7981b96ad1ec0c0da15cf249fb
manbalboy <manbalboy@hanmail.net>
2021-02-14 17:38:19 +0900

    TODOLIST 구현

--- vue-todo-manbalboy/src/components/TodoList.vue
+++ vue-todo-manbalboy/src/components/TodoList.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
-        todoList
+        <ul>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+                {{todoItem}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-export default {
+    export default {
+        // created vue 라이프사이클 인스턴스 생성될때 한번 호출
+        created : function () {
+            if (localStorage.length > 0) {
+                for (var i = 0; i < localStorage.length; i++) {
+                    if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                        this.todoItems.push(localStorage.key(i));
+                    }
+                }
+            }
+        },
 
-}
+        data : function () {
+            return  {
+                todoItems : []
+            }
+        }
+    }
 </script>
 
 <style>
