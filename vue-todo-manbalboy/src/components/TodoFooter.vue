77b6ed18f4c009b9e874d0e0f8728eef055fec3c
manbalboy <manbalboy@hanmail.net>
2021-02-14 18:37:32 +0900

    footer UI 구현

--- vue-todo-manbalboy/src/components/TodoFooter.vue
+++ vue-todo-manbalboy/src/components/TodoFooter.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        footer
+    <div class="clearAllConTainer">
+       <span class="clearAllBtn" v-on:click="clearTodo">
+           <b>Clear All</b>
+       </span>
+       <p class="prodBy">prod by manbalboy</p>
     </div>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        methods: {
+            clearTodo : function  () {
+                localStorage.clear();
+            }
+        },
+    }
 </script>
 
-<style>
+<style scoped>
+    .clearAllConTainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
 
+    .clearAllBtn {
+        color: #e20303;
+        display: block;
+    }
+
+    .prodBy {
+        color: #9ba1ac;
+    }
 </style>
