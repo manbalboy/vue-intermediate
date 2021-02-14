9704caaccbd8a638dfa6b16d356f564f86c34a07
manbalboy <manbalboy@hanmail.net>
2021-02-14 23:00:08 +0900

    트랜지션 사용자 UI 개선 구현

--- vue-todo-manbalboy/src/components/TodoList.vue
+++ vue-todo-manbalboy/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ul>
+        <transition-group name="list" tag="ul">
             <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
                 <i class="fas fa-check checkBtn" v-bind:class="{checkBtnCompleted : todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
                 <span v-bind:class="{textCompleted : todoItem.completed}">
@@ -10,7 +10,7 @@
                     <i class="fas fa-trash-alt"></i>
                 </span>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
@@ -68,5 +68,12 @@
         margin-left: auto;
     }
 
-
+    /* 리스트 트랜지션 */
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+    .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+        opacity: 0;
+        transform: translateY(30px);
+    }
 </style>
