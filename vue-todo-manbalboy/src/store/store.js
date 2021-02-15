236cbd3573fee54a889f0dcd154680b089c10bee
manbalboy <manbalboy@hanmail.net>
2021-02-15 12:32:45 +0900

    vuex 적용 실습

---
+++ vue-todo-manbalboy/src/store/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    // Vuex 기술 요소 
+
+    // state : 여러 컴퍼넌트에 공유되는 데이터 data
+    // getters : 연산된 state 값을 접근하는 속성 computed
+    // mutations : state 값을 변경하는 이벤트 로직 메서드 methods
+    // action : 비동기 처리 로직을 선언하는 메서드 aysnc methods
+    
+});
